6f838e2282ad7724a8ab05474f2a5cd580b2902b
Denys <s33928@pjwstk.edu.pl>
2026-03-14 16:47:58 +0100

    Implement binary export for NeuralCanvas with dimension metadata.

---
+++ src/io/FileManager.java
@@ -0,0 +1,30 @@
+package io;
+
+import model.NeuralCanvas;
+
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileManager {
+
+    public void saveToBinary(NeuralCanvas canvas,String path){
+
+        try(DataOutputStream dataOutputStream = new DataOutputStream(new FileOutputStream(path))) {
+
+            dataOutputStream.writeInt(canvas.getWidth());
+            dataOutputStream.writeInt(canvas.getHeight());
+
+            for (int y = 0; y < canvas.getHeight(); y++)
+                for (int x = 0; x < canvas.getWidth(); x++) dataOutputStream.writeInt(canvas.getColor(x, y));
+
+
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+
+}
